2d57318bc7b221dd6ec2a40634c4aa38244d3632
spaceman1412 <spacemannormal@gmail.com>
2022-03-22 15:21:23 +0700

    Set event for 0-9 and Clear button

--- app/src/main/java/hcmute/edu/vn/calculator_03/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/calculator_03/MainActivity.java
@@ -3,12 +3,41 @@ package hcmute.edu.vn.calculator_03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String textOnCalculator = "0";
+    TextView textViewOnCalculator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewOnCalculator = findViewById(R.id.txt_number);
     }
+
+    public void Click(View view) {
+        Button button = (Button) view;
+        textOnCalculator = textViewOnCalculator.getText().toString();
+        if (!textOnCalculator.equals("0")) {
+            String textOnButton = textOnCalculator + button.getText().toString();
+            textViewOnCalculator.setText(textOnButton);
+        } else {
+            String textOnButton = button.getText().toString();
+            textViewOnCalculator.setText(textOnButton);
+        }
+    }
+
+    public void ClearClick(View view)
+    {
+        Button button = (Button) view;
+        textViewOnCalculator.setText("0");
+    }
+
+
 }
